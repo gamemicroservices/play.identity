d95cea4dba09ec499e11ed4371eed6934e088a3c
André <55090443+andretheprogrammer@users.noreply.github.com>
2022-07-23 16:42:27 +0200

    Delete Play.Trading directory

--- Play.Trading/src/Play.Trading.Service/Settings/QueueSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Play.Trading.Service.Settings
-{
-    public class QueueSettings
-    {
-        public string GrantItemsQueueAddress { get; init;}
-        public string DebitGilQueueAdress { get; init; }
-        public string SubtractItemsQueueAddress { get; init; }
-    }
-}
